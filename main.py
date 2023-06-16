32af4030add65197c3d4c19ab108999b9be9bb75
phamdinhdatai <datolg123@gmail.com>
2023-06-17 00:26:14 +0700

    updated main file

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from utils import plot_performence, plot_cf
 from sklearn.metrics import classification_report
 static_path = 'data\static'
 dynamic_path = 'data\dynamic'
-label_dynamic_names = ["Prone", "Lateral Left", "Lateral Right", "Supine"]
+label_names = ["Prone", "Lateral Left", "Lateral Right", "Supine"]
 def load_data(data_type):
     # load data 
     if data_type == 'static':# data with person have no actitity when sleep 
@@ -47,6 +47,7 @@ def parse_opt(known=False):
     parser.add_argument('--num_classes', type=int, default=5, help='numbers of classes in the dataset')
     parser.add_argument('--epochs', type=int, default=100, help='epochs')
     parser.add_argument('--sequence_lenght', type=int, default=None, help='sequence_lenght for Sequence model')
+    parser.add_argument('--overlap', type=float, default=None, help='overlap of window across samples')
     parser.add_argument('--batch_size', type=int, default=128, help='setting batch_size')
     parser.add_argument('--plot', type=bool, default=True, help='plot performance')
     return parser.parse_known_args()[0] if known else parser.parse_args()
@@ -57,7 +58,9 @@ def parse_opt(known=False):
 opt = parse_opt(True)
 data_type = opt.data_type
 sequence_length = opt.sequence_lenght
+overlap = opt.overlap
 
+steps = int(sequence_length - overlap * sequence_length)
 # set number of classes in each model
 MLP_config['n_classes'] =  opt.num_classes 
 CNN_config['n_classes'] =  opt.num_classes 
@@ -93,9 +96,9 @@ if opt.sequence_lenght is not None:
     Transformer_config['timestep'] = opt.sequence_lenght
     CNN_config["timestep"] = opt.sequence_lenght
 
-    train_X, sequence_labels_train = generate_data(train_scaled, train_label, sequence_length= sequence_length)
-    val_X, sequence_labels_val = generate_data(val_scaled, val_label, sequence_length= sequence_length)
-    test_X, sequence_labels_test = generate_data(test_scaled, test_label, sequence_length= sequence_length)
+    train_X, sequence_labels_train = generate_data(train_scaled, train_label, sequence_length= sequence_length, step= steps)
+    val_X, sequence_labels_val = generate_data(val_scaled, val_label, sequence_length= sequence_length, step= steps)
+    test_X, sequence_labels_test = generate_data(test_scaled, test_label, sequence_length= sequence_length, step= steps)
     train_y = to_categorical(sequence_labels_train)
     val_y = to_categorical(sequence_labels_val)
     test_y = to_categorical(sequence_labels_test)
@@ -117,7 +120,14 @@ else:
 
 print("shape of training data: ", train_X.shape)
 print("shape of testing data: ", test_X.shape)
+print("shape of training data: ", val_X.shape)
 print("shape of training label: ", train_y.shape)
+print("shape of test label: ", test_y.shape)
+print("shape of valid label: ", val_y.shape)
+
+
+
+
 
 
 def main(opt):
@@ -178,20 +188,20 @@ def main(opt):
 if __name__ == "__main__":
     opt = parse_opt(True)
     main(opt)
-# file_path = r"C:\Users\TAOSTORE\Desktop\SPP\data\SPP_Data/"
+# # file_path = r"C:\Users\TAOSTORE\Desktop\SPP\data\SPP_Data/"
 
 
-# from preprocess.processing import load_regr_data
+# # from preprocess.processing import load_regr_data
 
-# data, labels = load_regr_data(file_path=file_path)
+# # data, labels = load_regr_data(file_path=file_path)
 
-# print("shape of data: ",data.shape)
-# print(labels)
+# # print("shape of data: ",data.shape)
+# # print(labels)
 
 
 
-# from model.multi_task_model import SPPNet
-# from model.cnn_lstm import CNN_LSTM
+# # from model.multi_task_model import SPPNet
+# # from model.cnn_lstm import CNN_LSTM
 
-# model = CNN_LSTM()
-# print(model.summary())
+# # model = CNN_LSTM()
+# # print(model.summary())
