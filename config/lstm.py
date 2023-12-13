b7adb172d3c7cd251ec28579cf5f2b7ad8a8648d
Pham Dinh Dat <74858331+phamdinhdat-ai@users.noreply.github.com>
2023-12-13 14:34:53 +0700

    Update lstm.py

--- config/lstm.py
+++ config/lstm.py
@@ -3,8 +3,8 @@ class Config:
     timestep  = 20
     n_features = 3
     n_classes  = 13
-    hidden_size = [128, 64]
-    mlp_units  = [32]
+    hidden_size = [8, 16]
+    mlp_units  = [16]
     dropout = 0.4 
     log_dir = "checkpoint/lstm/"
     save_file =  "checkpoint/lstm_model/"
