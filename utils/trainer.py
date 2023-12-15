303ea6ce7431c525595abe86e8d2bc1f6576f430
phamdinhdatai <datolg123@gmail.com>
2023-12-16 03:03:23 +0700

    update file

--- utils/trainer.py
+++ utils/trainer.py
@@ -199,14 +199,14 @@ def train_model(model,
             model.save(best_weights)
             min_val_loss = float(loss_e_val/total_val)
 
-        if epoch%10 == 0:
-            filename = "./work_dir/hist_{}_{}_{}_{}_{}/training_history_{}_{}_{}_{}_{}.pkl".format(arg.model_type, arg.data_type, arg.sequence_length, arg.overlap,arg.scenario, arg.model_type,epoch, today, arg.loss_fn, arg.normalizer)
-            checkpoint_pth = f"./checkpoint/checkpoint_{arg.model_type}_{arg.data_type}_{arg.sequence_length}_{arg.overlap}_{ arg.sequence_length}_{arg.loss_fn}_{arg.normalizer}/{arg.model_type}_{epoch}.keras"
-            os.makedirs(os.path.dirname(checkpoint_pth), exist_ok=True)
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            model.save(checkpoint_pth)
-            with open("./work_dir/hist_{}_{}_{}_{}_{}/last_training_history_{}_{}_{}_{}_{}.pkl".format(arg.model_type, arg.data_type, arg.sequence_length, arg.overlap,arg.scenario, arg.model_type,epoch, today, arg.loss_fn, arg.normalizer), 'wb') as  f:
-                pickle.dump(history, f)
+        # if epoch%10 == 0:
+        #     filename = "./work_dir/hist_{}_{}_{}_{}_{}/training_history_{}_{}_{}_{}_{}.pkl".format(arg.model_type, arg.data_type, arg.sequence_length, arg.overlap,arg.scenario, arg.model_type,epoch, today, arg.loss_fn, arg.normalizer)
+        #     checkpoint_pth = f"./checkpoint/checkpoint_{arg.model_type}_{arg.data_type}_{arg.sequence_length}_{arg.overlap}_{ arg.sequence_length}_{arg.loss_fn}_{arg.normalizer}/{arg.model_type}_{epoch}.keras"
+        #     os.makedirs(os.path.dirname(checkpoint_pth), exist_ok=True)
+        #     os.makedirs(os.path.dirname(filename), exist_ok=True)
+        #     model.save(checkpoint_pth)
+        #     with open("./work_dir/hist_{}_{}_{}_{}_{}/last_training_history_{}_{}_{}_{}_{}.pkl".format(arg.model_type, arg.data_type, arg.sequence_length, arg.overlap,arg.scenario, arg.model_type,epoch, today, arg.loss_fn, arg.normalizer), 'wb') as  f:
+        #         pickle.dump(history, f)
                 
     return history, model
 
