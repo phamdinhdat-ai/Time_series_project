f802412abd491e5d0363d730e6f09158bfa42701
Pham Dinh Dat <74858331+phamdinhdat-ai@users.noreply.github.com>
2024-01-20 11:56:00 +0700

    Update cnn.py

--- config/cnn.py
+++ config/cnn.py
@@ -1,10 +1,10 @@
 class Config:
     timestep  = 20
     n_features = 3
-    filters = [128, 64]
+    filters = [8, 16]
     kernel_size = 3
     n_classes  = 13
-    mlp_units  = [32]
+    mlp_units  = [16]
     dropout = 0.4 
     log_dir = "checkpoint/lstm/"
     save_file =  "checkpoint/lstm_model/"
