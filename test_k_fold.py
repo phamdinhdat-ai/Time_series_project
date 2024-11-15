9d7b95640906dcd6dfea7fe6914efdd71c485111
phamdinhdat-ai <datolg123@gmail.com>
2024-11-16 04:52:41 +0700

    update training kfold

---
+++ test_k_fold.py
@@ -0,0 +1,419 @@
+import os
+import shutil
+from sklearn.model_selection import KFold
+import numpy as np
+import shutil
+import pickle
+import os
+import pandas as pd 
+import numpy as np
+import tensorflow as tf
+from model.lstm import LSTM
+from model.adaptive_lstm import AdaptiveLSTM
+from config.lstm import Config 
+from config.lstm import Config
+# from utils.trainer import train_model
+from utils.load_data import load_and_process_data, tensorflow_dataset, load_from_folder
+# from utils.trainer import test_model
+from utils.parse import parse_opt
+from utils.plot import plot_performance
+from utils.trainer import train_model
+from utils.losses import negative_log_likelihood
+from utils.trainer import test_model
+from datetime import date
+from utils.trainer import experiment
+
+
+
+
+opt = parse_opt(True)
+print("=" * 50)
+for arg in vars(opt):
+    print(arg, '=',getattr(opt, arg))
+print("=" * 50)
+np.random.seed(opt.seed)
+tf.random.set_seed(opt.seed)
+print("Original Seed: ", opt.seed)
+print(tf.__version__)
+
+today = str(date.today())
+folder = 'data/new_data_static/all_person/'
+K = opt.k_fold
+
+test_neck_path = 'data/necktest'
+k_folds_dir = "data/new_data_static/kfold_data"
+os.makedirs(k_folds_dir, exist_ok=True)
+list_paths = os.listdir(folder)
+
+kf = KFold(n_splits=K, shuffle=True, random_state=42)
+BATCH_SIZE = opt.batch_size
+EPOCHS = opt.epochs
+
+model_type = opt.model_type
+data_type = opt.data_type
+lossfn_str = opt.loss_fn
+#set up training
+optimizer = tf.keras.optimizers.Adam()
+if lossfn_str =='ce':
+  loss_fn = tf.keras.losses.CategoricalCrossentropy()
+elif lossfn_str =='nll':
+  loss_fn = negative_log_likelihood
+elif lossfn_str == 'cfe':
+    loss_fn = tf.keras.losses.CategoricalFocalCrossentropy()
+elif lossfn_str == 'kl':
+    loss_fn = tf.keras.losses.KLDivergence()
+elif lossfn_str == 'ch':
+    loss_fn = tf.keras.losses.CategoricalHinge()
+else:
+  loss_fn = tf.keras.losses.CategoricalCrossentropy()
+if model_type == "lstm":
+    from model.lstm import LSTM
+    from config.lstm import Config
+    #set up training
+    # load model with configure
+    config = Config
+    config.normalizer = opt.normalizer
+    config.n_classes = opt.num_classes 
+    config.timestep = opt.sequence_length
+    model_lstm = LSTM(config=config)
+    model = model_lstm.build()
+    
+if model_type =='cnn':
+    #set up training
+    # load model with configure
+    from model.cnn import CNN
+    from config.cnn import Config
+    config = Config
+    config.n_classes = opt.num_classes 
+    config.timestep = opt.sequence_length
+    config.kernel_size = opt.kernel_size 
+    # config.filters = opt.filters
+    config.normalizer = opt.normalizer
+    model_cnn = CNN(config=config)
+    model = model_cnn.build()
+    
+if model_type == 'mlp':
+    from model.mlp import MLP
+    from config.mlp import Config
+    config  = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.normalizer = opt.normalizer
+    model_mlp = MLP(config=config)
+    model = model_mlp.build()
+    
+    
+if model_type == 'baseline':
+    from model.lstm_baseline import LSTM_baseline
+    from config.lstm_baseline import Config
+    
+    config  = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    model_baseline = LSTM_baseline(config=config)
+    model = model_baseline.build()
+    
+    
+    
+if model_type == 'adaptive_lstm':
+    from model.adaptive_lstm import AdaptiveLSTM
+    from config.adaptive_lstm import Config
+    config  = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.filters = opt.filters
+    config.kernel_size = opt.kernel_size
+    config.normalizer = opt.normalizer
+    model_adaptive = AdaptiveLSTM(config=config)
+    model = model_adaptive.build()
+    
+    
+    
+if model_type == "transformers":
+    from model.transformer import Transformer
+    from config.transformers import Config
+    config = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.filter = opt.filters
+    config.normalizer = opt.normalizer
+    model_transformer = Transformer(config=config)
+    model = model_transformer.build()
+    
+if model_type == "cnn_bilstm":
+    from model.cnn_bilstm import CNNBiLSTM
+    from config.cnn_bilstm import Config
+    config  = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.filters = opt.filters
+    config.kernel_size = opt.kernel_size
+    config.normalizer = opt.normalizer
+    model_cnnlstm = CNNBiLSTM(config=config)
+    model = model_cnnlstm.build()
+
+if model_type == "cnn_bilstm_att":
+    from model.cnn_bilstm_att import CNNBiLSTMAttention
+    from config.cnn_bilstm import Config
+    
+    config = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.filters = opt.filters
+    config.kernel_size = opt.kernel_size
+    config.normalizer = opt.normalizer
+    model_cnnlstmatt = CNNBiLSTMAttention(config=config)
+    model = model_cnnlstmatt.build()
+
+if model_type   == "cnn_gru":
+    from model.cnn_gru import CNNGRU
+    from config.cnn_bilstm import Config
+    config  = Config
+    config.n_classes = opt.num_classes
+    config.timestep  = opt.sequence_length
+    config.filters = opt.filters
+    config.kernel_size = opt.kernel_size
+    config.normalizer = opt.normalizer
+    model_cnngru = CNNGRU(config=config)
+    model = model_cnngru.build()
+    
+
+
+if opt.check_point is not None:
+    model = tf.keras.models.load_model(str(opt.check_point))
+print(model.summary())
+
+
+for fold_idx, (train_idx, val_idx) in enumerate(kf.split(list_paths)):
+    print("Fold Id: ", fold_idx)
+    print("Train Folder:  ", train_idx)
+    print("Val Folder:  ", val_idx)
+    
+    k_folds_dir_i = f'{k_folds_dir}/K_fold_{fold_idx}'
+    os.makedirs(k_folds_dir_i, exist_ok=True)
+    train_folder = f'{k_folds_dir_i}/trainset'
+    test_folder  = f'{k_folds_dir_i}/testset'
+    os.makedirs(train_folder, exist_ok=True)
+    os.makedirs(test_folder, exist_ok=True)    
+    for id in train_idx:
+        source_path = f'{folder}/{list_paths[id]}'
+        destination_path = f'{train_folder}/{list_paths[id]}'
+        if os.path.isdir(source_path):
+            # Recursively copy subdirectories
+            shutil.copytree(source_path, destination_path, dirs_exist_ok=True)
+        else:
+            # Copy individual files
+            shutil.copy2(source_path, destination_path)
+    
+        # print(f"Contents of '{folder}' have been copied to '{train_folder}'.")
+        
+        
+    for id in val_idx: 
+        source_path = f'{folder}/{list_paths[id]}'
+        destination_path = f'{test_folder}/{list_paths[id]}'
+        if os.path.isdir(source_path):
+            # Recursively copy subdirectories
+            shutil.copytree(source_path, destination_path, dirs_exist_ok=True)
+        else:
+            # Copy individual files
+            shutil.copy2(source_path, destination_path)
+    
+        # print(f"Contents of '{folder}' have been copied to '{test_folder}'.")
+    
+    
+    
+    print("<=====> Training progress <=====>")
+    if opt.scenario == "sample_divide":
+        print("Combine all data of 16 person and split with ratio at 0.75-0.25 (12-4) for train/test")
+        X_train, X_val, y_train, y_val = load_and_process_data(file_path='./data/dataset/trainset_16.npy', sequence_length=config.timestep, overlap=opt.overlap, valid_ratio=0.2)
+    else:
+        X_train, X_val, y_train, y_val = load_from_folder(folder_path=train_folder, sequence_length=config.timestep, overlap=opt.overlap, valid_ratio=0.2)
+    print("shape of training data: ", X_train.shape)
+    print("shape of validation data: ", X_val.shape)
+    
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(BATCH_SIZE)
+    val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
+    val_dataset = val_dataset.batch(BATCH_SIZE)
+
+    print("===Load data passed====")
+    #set up training
+    #set up optimizer  chosen by implement in configure
+    optimizer = tf.keras.optimizers.Adam()
+    # training progress
+    print("Training Model ........")
+    history, model = train_model(model=model, dataset=train_dataset, loss_fn=loss_fn, optimizer=optimizer,epochs=EPOCHS, val_dataset=val_dataset, batch_size=BATCH_SIZE,  arg=opt)
+    print("=====Training Done !====")
+    history["test"]  = dict()
+    #test progress
+    print(loss_fn.__class__.__name__)
+    if opt.scenario == "sample_divide":
+        scenario = opt.scenario
+        print("<====== Evaluate on testset =======>")
+        X_test, y_test = load_and_process_data(file_path='./data/dataset/testset_16.npy',sequence_length= opt.sequence_length, overlap = opt.overlap, valid_ratio=None)
+        ds_test = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+        ds_test = ds_test.batch(BATCH_SIZE)
+        cl_report, cf_matrix, results = test_model(test_set=ds_test, model=model, loss_fn = loss_fn, batch_size=BATCH_SIZE)
+        print("Result on testste: \n", results)
+        metrics = ["Loss", "Acc", "F1-Score", "Lipschitz Loss", "Lipshitz model", "Time"]
+        history["test"]['cl_report'] = cl_report
+        history["test"]['cf_matrix'] = cf_matrix      
+        for metric, result in zip(metrics, results):
+            history["test"][metric] = result
+
+
+        cols = ["Person", "Loss", "F1-Score", "Acc", "L Loss", "L model"]
+            
+        df_total = dict()
+        df_total['Person'] = "Eval All"
+        for i in range(4):
+            df_total[cols[i+1]] = [results[i]]
+        
+        
+        file_excel_total = "./work_dir/hist_{}_{}_{}_{}_{}_{}_{}/test_total_history_{}_{}_{}_{}_{}_{}.csv".format(model_type, data_type, opt.sequence_length, opt.overlap,scenario,lossfn_str, opt.normalizer, EPOCHS, BATCH_SIZE,  scenario, today, lossfn_str, opt.normalizer)
+        
+        # file_excel_total = "./work_dir/hist_{}_{}_{}_{}_{}_{}_{}/test_history_eval_total_{}_{}_{}_{}_{}_{}.csv".format(model_type, data_type, opt.sequence_length, opt.overlap,scenario,lossfn_str, opt.normalizer, EPOCHS, BATCH_SIZE,  scenario, today, lossfn_str, opt.normalizer)
+        os.makedirs(os.path.dirname(file_excel_total), exist_ok=True)
+        df = pd.DataFrame(df_total, columns=cols)
+        df.to_csv(file_excel_total)
+        print("test_done!!!!!")
+
+    
+        history["test_neck"] = dict()
+        for file in os.listdir(test_neck_path):  
+            name = file.split("_")[-1]
+            print(f"<=======>Test on {name[:-4]}'s data<=======>\n")
+            file_dir = f"{test_neck_path}/{file}"
+            X_test, y_test = load_and_process_data(file_path=file_dir,sequence_length= opt.sequence_length, overlap = opt.overlap, valid_ratio=None)
+            ds_test = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+            ds_test = ds_test.batch(BATCH_SIZE)
+            cl_report, cf_matrix,results_neck = test_model(test_set=ds_test, model=model, loss_fn = loss_fn, batch_size=BATCH_SIZE)
+            print(f"Result on {name}'s data(neckdata): \n", results_neck)
+            metrics = ["Loss", "Acc","F1-Score", "Lipschitz Loss", "Lipshitz model", "Time"]
+            history["test_neck"][name[:-4]] = dict()
+            history["test_neck"][name[:-4]]['cl_report'] = cl_report
+            history["test_neck"][name[:-4]]['cf_matrix'] = cf_matrix
+            # experiment.log_confusion_matrix(
+            #         cm=cf_matrix)    
+            for metric, result in zip(metrics, results_neck):
+                history["test_neck"][name[:-4]][metric] = result
+                
+            
+            
+    else:
+        for file in os.listdir(test_folder):  
+            scenario = 'person_divide'
+            name = file.split("_")[-1]
+            print(f"<=======>Test on {name[:-4]}'s data<=======>\n")
+            file_dir = f"{test_folder}/{file}"
+            X_test, y_test = load_and_process_data(file_path=file_dir,sequence_length= opt.sequence_length, overlap = opt.overlap, valid_ratio=None)
+            ds_test = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+            ds_test = ds_test.batch(BATCH_SIZE)
+            cl_report, cf_matrix,results = test_model(test_set=ds_test, model=model, loss_fn = loss_fn, batch_size=BATCH_SIZE)
+            print(f"Result on {name}'s data: \n", results)
+            metrics = ["Loss", "Acc","F1-Score", "Lipschitz Loss", "Lipshitz model", "Time"]
+            history["test"][name[:-4]] = dict()
+            history["test"][name[:-4]]['cl_report'] = cl_report
+            history["test"][name[:-4]]['cf_matrix'] = cf_matrix   
+            for metric, result in zip(metrics, results):
+                history["test"][name[:-4]][metric] = result
+                
+        history["test_neck"] = dict()
+        for file in os.listdir(test_neck_path):  
+            scenario = 'person_divide'
+            name = file.split("_")[-1]
+            print(f"<=======>Test on {name[:-4]}'s data<=======>\n")
+            file_dir = f"{test_neck_path}/{file}"
+            X_test, y_test = load_and_process_data(file_path=file_dir,sequence_length= opt.sequence_length, overlap = opt.overlap, valid_ratio=None)
+            ds_test = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+            ds_test = ds_test.batch(BATCH_SIZE)
+            cl_report, cf_matrix,results = test_model(test_set=ds_test, model=model, loss_fn = loss_fn, batch_size=BATCH_SIZE)
+            print(f"Result on {name}'s data(neckdata): \n", results)
+            metrics = ["Loss", "Acc", "F1-Score","Lipschitz Loss", "Lipshitz model", "Time"]
+            history["test_neck"][name[:-4]] = dict()
+            history["test_neck"][name[:-4]]['cl_report'] = cl_report
+            history["test_neck"][name[:-4]]['cf_matrix'] = cf_matrix
+            # experiment.log_confusion_matrix(
+            #         cm=cf_matrix)    
+            for metric, result in zip(metrics, results):
+                history["test_neck"][name[:-4]][metric] = result
+            
+        import numpy as np
+        arr  = np.zeros((3,5))
+        persons = []
+        for i, (person, metrics) in enumerate(history["test"].items()):
+            print(person, metrics)
+            arr[i][0]  = metrics["Loss"]
+            arr[i][1] = metrics["Acc"]
+            arr[i][2] = metrics["F1-Score"]
+            arr[i][3] = metrics["Lipschitz Loss"]
+            arr[i][4] = metrics["Lipshitz model"]
+            persons.append(person)
+
+
+        mean = np.mean(arr, axis = 0 ).reshape(1, 5)
+        std =  np.std(arr, axis = 0).reshape(1,5)
+        print("mean_exp", mean)
+        print("std_exp", std)
+        persons.append("Mean")
+        persons.append("Standard Deviation")
+        df_np = np.concatenate([arr, mean, std], axis = 0 )
+        cols = ["Person", "Loss","F1-Score", "Acc", "L Loss", "L model"]
+        df_dict = dict()
+        df_dict["Person"] = persons
+        for i in range(5):
+            df_dict[cols[i+1]] = df_np[:, i]
+
+
+        # save test history on excel file
+        file_excel = "./work_dir/hist_seed{}_{}_{}_{}_{}_{}_{}_{}/test_history_{}_{}_{}_{}_{}_{}_seed{}.csv".format(opt.seed,model_type, data_type, opt.sequence_length, opt.overlap,scenario,lossfn_str, opt.normalizer, EPOCHS, BATCH_SIZE,  scenario, today, lossfn_str, opt.normalizer, opt.seed)
+        os.makedirs(os.path.dirname(file_excel), exist_ok=True)
+        df = pd.DataFrame(df_dict, columns=cols)
+        df.to_csv(file_excel)
+
+
+
+
+    import numpy as np
+    arr_neck  = np.zeros((2, 5))
+    cols = ["Person", "Loss","F1-Score", "Acc", "L Loss", "L model"]
+    persons_neck = []
+    for i, (person, metrics) in enumerate(history["test_neck"].items()):
+        print(person, metrics)
+        arr_neck[i][0]  = metrics["Loss"]
+        arr_neck[i][1] = metrics["Acc"]
+        arr_neck[i][2] = metrics["F1-Score"]
+        arr_neck[i][3] = metrics["Lipschitz Loss"]
+        arr_neck[i][4] = metrics["Lipshitz model"]
+        persons_neck.append(person)
+        
+    print(arr_neck)
+    df_neck = dict()
+    df_neck['Person'] = persons_neck
+    for i in range(5):
+        df_neck[cols[i+1]] = arr_neck[:, i]
+    
+    
+    file_excel_neck = "./work_dir/hist_seed{}_fold_index{}_{}_{}_{}_{}_{}_{}_{}/test_neck_history_{}_{}_{}_{}_{}_{}.csv".format(opt.seed, fold_idx , model_type, data_type, opt.sequence_length, opt.overlap,scenario,lossfn_str, opt.normalizer, EPOCHS, BATCH_SIZE,  scenario, today, lossfn_str, opt.normalizer)
+    os.makedirs(os.path.dirname(file_excel_neck), exist_ok=True)
+    df_neck_data = pd.DataFrame(df_neck, columns=cols)
+    df_neck_data.to_csv(file_excel_neck)
+
+
+
+    filename = "./work_dir/hist_seed{}_fold_index{}_{}_{}_{}_{}_{}_{}_{}/training_history_{}_{}_{}_{}_{}_{}_seed{}.pkl".format(opt.seed,fold_idx, model_type, data_type, opt.sequence_length, opt.overlap,scenario,lossfn_str, opt.normalizer, EPOCHS, BATCH_SIZE,  scenario, today, lossfn_str, opt.normalizer, opt.seed)
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'wb') as  f:
+        pickle.dump(history, f)   
+
+    print("=" * 50)
+    print(f"Fold {fold_idx}:Training Done...")
+    print("=" * 50)
+
+if __name__ == "__main__":
+    opt = parse_opt(True)
+
+    print("=" * 50)
+    for arg in vars(opt):
+        print(arg, '=',getattr(opt, arg))
+    print("=" * 50)
