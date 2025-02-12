69625c867106e5bc9778497cdeb4cdf1be5ffb10
phamdinhdatai <datolg123@gmail.com>
2025-02-12 07:59:01 +0700

    update seed run

--- run_seed.sh
+++ run_seed.sh
@@ -15,9 +15,17 @@
 
 pip install -r requirements.txt
 
-MAX_SEED=50
-for ((i=1; i<=$MAX_SEED; i++))
-do
-    python main.py --model_type adaptive_lstm --data_type static --scenario person_divide --num_classes 12 --epochs 300 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed $i
+# MAX_SEED=50
+# for ((i=1; i<=$MAX_SEED; i++))
+# do
+#     python main.py --model_type adaptive_lstm --data_type static --scenario person_divide --num_classes 12 --epochs 300 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed $i
 
-done
+# done
+
+python main.py --model_type cnn_gru_bilstm --data_type static --scenario person_divide --num_classes 12 --epochs 2 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed 50
+
+python main.py --model_type cnn_bilstm_gru --data_type static --scenario person_divide --num_classes 12 --epochs 2 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed 50
+
+python main.py --model_type cnn_gru_att --data_type static --scenario person_divide --num_classes 12 --epochs 2 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed 50
+
+python main.py --model_type cnn_gru_bilstm_att --data_type static --scenario person_divide --num_classes 12 --epochs 2 --sequence_length 100 --overlap 0.4 --batch_size 512 --loss_fn nll  --normalizer batch_norm --seed 50
